a6a5482fc1ab6a7597605c0c9a6739a3b22ce6c5
tomoyk <tmyk.kym@gmail.com>
2017-06-04 22:13:27 +0900

    first commit

---
+++ orsh_sample.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+
+  char *line;
+  char *cmd = "exit";
+
+  do{
+    printf("[root@localhost /]# ");
+    // fgets(line, sizeof(line), stdin);
+
+    if( strcmp( line, cmd) == 0){
+      return 0;
+    }
+
+  }while(fgets(line, sizeof(line), stdin) );
+
+  return 0;
+}
