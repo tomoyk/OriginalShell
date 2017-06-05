e8486a38afc51b934897261698e0a9b5661eabd5
tomoyk <tmyk.kym@gmail.com>
2017-06-05 12:59:44 +0900

    add a program of open()

---
+++ dev/2-2-1_open.c
@@ -0,0 +1,15 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+// int open(const char *name, int flags);
+// int open(const char *name, int flags, mode_t mode);
+
+//int main(void){
+
+  int fd = open("/home/test01/aa", O_RDONLY);
+
+
+
+
+//}
