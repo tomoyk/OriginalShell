29df068bf18a73e9afc2a7cca31c5bc12e1ee2ad
tomoyk <tmyk.kym@gmail.com>
2017-06-05 18:21:36 +0900

    tried open()

--- dev/2-2-1_open.c
+++ dev/2-2-1_open.c
@@ -5,11 +5,16 @@
 // int open(const char *name, int flags);
 // int open(const char *name, int flags, mode_t mode);
 
-//int main(void){
+int main(void){
 
-  int fd = open("/home/test01/aa", O_RDONLY);
+  // int fd = open("/home/test01/aa", O_RDONLY | );
+  int fd = open("/home/test01/aa", O_WRONLY | O_TRUNC );
+  if(fd == -1){
+    // Error
+  }
+
+  
 
 
-
-
-//}
+  return 0;
+}
